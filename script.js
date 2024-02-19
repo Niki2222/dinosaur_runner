7d1b39d3609eaf0a45db4ce219741a53d5a58cd5
Niki2222 <89404731+Niki2222@users.noreply.github.com>
2024-02-19 12:57:14 +0200

    collision and game over update

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const DOWN_KEY = 40;
 let upKeyPress = false;
 let downKeyPress = false;
 let time = 0;
+let screenEnd = 710;
+let intervalIdTime;
+let intervalIdObstacle;
+let intervalIdObstacleBird;
+let newObstacle = document.createElement("div");
+let newObstacleBird = document.createElement("div");
 
 function startGame() {
     document.addEventListener("keydown", keyPressed);
@@ -20,7 +26,7 @@ function startGame() {
 }
 
 function updateTime() {
-    let intervalIdTime = setInterval(function () {
+    intervalIdTime = setInterval(function () {
         ++time;
         document.getElementById("score").innerHTML = time;
     }, 1000);
@@ -88,8 +94,6 @@ function createTrack() {
     mainDino.appendChild(newTrack);
 }
 
-let newObstacle = document.createElement("div");
-let newObstacleBird = document.createElement("div");
 function createObstacle(name, width, height, bottomPos, rightPos) {
     name.style.width = `${width}px`;
     name.style.height = `${height}px`;
@@ -101,29 +105,69 @@ function createObstacle(name, width, height, bottomPos, rightPos) {
 }
 
 function obstacleMovement() {
-    let position1 = 0;
+    let rightPosGround = 0;
     let rightPosBird = 0;
-    createObstacle(newObstacle, 30, 30, 19, 0);
     setTimeout(function () {
         createObstacle(newObstacleBird, 30, 30, 60, 0);
-        let intervalIdObstacleBird = setInterval(function () {
+        intervalIdObstacleBird = setInterval(function () {
         rightPosBird += 1;
         newObstacleBird.style.right = `${rightPosBird}px`;
-        if (rightPosBird === 710) {
+        if (rightPosBird === screenEnd) {
             rightPosBird = 0;
         }
         }, 5);
     }, 2000);
-    let intervalIdObstacle = setInterval(function () {
-        position1 += 1;
-        newObstacle.style.right = `${position1}px`;
-        if (position1 === 710) {
-        position1 = 0;
+    createObstacle(newObstacle, 30, 30, 19, 0);
+    intervalIdObstacle = setInterval(function () {
+        rightPosGround += 1;
+        newObstacle.style.right = `${rightPosGround}px`;
+        if (rightPosGround === screenEnd) {
+            rightPosGround = 0;
         }
+        collision();
     }, 5);
 }
 
+function collisionGroundObs() {
+    let dinoRect = dinosaur.getBoundingClientRect();
+    let obstacleGroundRect = newObstacle.getBoundingClientRect();
+    if (dinoRect.bottom > obstacleGroundRect.top
+        && dinoRect.top < obstacleGroundRect.bottom
+        && dinoRect.right > obstacleGroundRect.left
+        && dinoRect.left < obstacleGroundRect.right) {
+        gameOver();
+    }
+}
+
+function collisionBirdObs() {
+    let dinoRect = dinosaur.getBoundingClientRect();
+    let obstacleBirdRect = newObstacleBird.getBoundingClientRect();
+    if (dinoRect.bottom > obstacleBirdRect.top
+        && dinoRect.top < obstacleBirdRect.bottom
+        && dinoRect.right > obstacleBirdRect.left
+        && dinoRect.left < obstacleBirdRect.right) {
+        gameOver();
+    }
+}
+
+function collision() {
+    collisionGroundObs();
+    collisionBirdObs();
+}
+
 function gameOver() {
-    
+    clearInterval(intervalIdTime);
+    clearInterval(intervalIdObstacle);
+    clearInterval(intervalIdObstacleBird);
+    let container = document.querySelector('.container');
+    let message = document.createElement('p');
+    message.innerText = "GAME OVER!!!";
+    message.style.fontSize = "4rem";
+    message.style.position = "absolute";
+    message.style.top = "30%";
+    message.style.left = "50%";
+    message.style.transform = "translate(-50%, -50%)";
+    message.style.textAlign = "center";
+    container.appendChild(message);
 }
 
